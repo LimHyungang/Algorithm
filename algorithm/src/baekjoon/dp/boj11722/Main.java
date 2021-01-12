9dba045cb502eea5b2c9c175181b6dd5829d7197
LimHyungang <hyungang7@gmail.com>
2021-01-12 10:59:39 +0900

    add boj 2156, 11053, 11055, 11722 source

---
+++ algorithm/src/baekjoon/dp/boj11722/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.dp.boj11722;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = 1;
+        for(int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(arr[j] > arr[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        OptionalInt ans = Arrays.stream(dp).max();
+        System.out.println(ans.getAsInt());
+    }
+}
